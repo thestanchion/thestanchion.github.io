76514739d164fc5506337ad43710ac1111027b1e
Ash Robbins <ashleyrobbins@redweb.com>
2018-01-31 15:54:35 +0000

    Initial nav in place and opening

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+/******************************************
+// Primary Navigation
+// Authors: Ash Robbins
+// Created date: 31/01/2017
+// Notes:
+******************************************/
+$(".c-nav-trigger").on("click", function() {
+    $(".c-nav").addClass("open");
+})
